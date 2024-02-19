0ea713efabc407828a75e64e68412aa83d7294ad
Sourabh Shinde <saurabh@Saurabhs-MacBook-Air.local>
2024-02-19 10:14:35 +0530

    error removed

--- divisibleTest.sh
+++ divisibleTest.sh
@@ -1,10 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 #Author :: Saurabh Shinde
 #Date :: 18 Feb 2024
 
-for i in {1..100};do
-        if [ $(expr $i % 3) == 0 ] && [ $(expr $i % 5) == 0 ]); then
-                echo $i
-        fi;
+for i in {1..100}; do
+    if [ $((i % 3)) == 0 ] && [ $((i % 5)) == 0 ]; then
+        echo $i
+    fi
 done
+
